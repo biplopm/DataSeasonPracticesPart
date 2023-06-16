90c9b79f57d8f614ac4c1c067ee50e1f8031bfb2
biplopm <kettyhomes6@gmail.com>
2023-06-16 14:18:18 +0600

    commit message

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const bloodGroup = prompt("Please enter a blood group");
+
+const result = person.filter((data) => data.bloodGroup === bloodGroup);
+
+console.log(result);
---
+++ js/data.js
@@ -0,0 +1,112 @@
+const person = [
+  {
+    id: "1",
+    name: "John",
+    age: "30",
+    location: "Mirpur",
+    bloodGroup: "O-",
+    gender: "Male",
+    phone: "01735147258",
+    lastDonateDate: "04 May 2022",
+    history: [
+      {
+        id: "1",
+        date: "23 January 2021",
+        name: "Ani",
+      },
+      {
+        id: "2",
+        date: "04 May 2022",
+        name: "Borish",
+      },
+    ],
+  },
+  {
+    id: "1",
+    name: "AAA",
+    age: "30",
+    location: "Mirpur",
+    bloodGroup: "O-",
+    gender: "Male",
+    phone: "01735147258",
+    lastDonateDate: "04 May 2022",
+    history: [
+      {
+        id: "1",
+        date: "23 January 2021",
+        name: "Ani",
+      },
+      {
+        id: "2",
+        date: "04 May 2022",
+        name: "Borish",
+      },
+    ],
+  },
+  {
+    id: "1",
+    name: "AAA",
+    age: "30",
+    location: "Mirpur",
+    bloodGroup: "O-",
+    gender: "Male",
+    phone: "01735147258",
+    lastDonateDate: "04 May 2022",
+    history: [
+      {
+        id: "1",
+        date: "23 January 2021",
+        name: "Ani",
+      },
+      {
+        id: "2",
+        date: "04 May 2022",
+        name: "Borish",
+      },
+    ],
+  },
+  {
+    id: "1",
+    name: "AAA",
+    age: "30",
+    location: "Mirpur",
+    bloodGroup: "O-",
+    gender: "Male",
+    phone: "01735147258",
+    lastDonateDate: "04 May 2022",
+    history: [
+      {
+        id: "1",
+        date: "23 January 2021",
+        name: "Ani",
+      },
+      {
+        id: "2",
+        date: "04 May 2022",
+        name: "Borish",
+      },
+    ],
+  },
+  {
+    id: "1",
+    name: "AAA",
+    age: "30",
+    location: "Mirpur",
+    bloodGroup: "O-",
+    gender: "Male",
+    phone: "01735147258",
+    lastDonateDate: "04 May 2022",
+    history: [
+      {
+        id: "1",
+        date: "23 January 2021",
+        name: "Ani",
+      },
+      {
+        id: "2",
+        date: "04 May 2022",
+        name: "Borish",
+      },
+    ],
+  },
+];
